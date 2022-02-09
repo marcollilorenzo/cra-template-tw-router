d28b367961bddd5ce2912f426f08ac6ab6c6d79b
Lorenzo Marcolli <lory.marcolli@gmail.com>
2022-02-09 17:49:05 +0100

    first commit

---
+++ template/src/App.js
@@ -0,0 +1,20 @@
+//router
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
+//pages
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ template/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-center font-bold text-7xl'>TW + Router</div>
+  )
+}
+
+export default Home
